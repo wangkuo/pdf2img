0abf917b968accce28e044365d673c485e3102ec
K.Wang <kuo.wang@outlook.com>
2025-02-12 11:37:59 +0800

    fix cmaps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/pdfjs-dist/cmaps/*',
+          dest: 'node_modules/pdfjs-dist/cmaps',
+        },
+      ],
+    }),
+  ],
 })
